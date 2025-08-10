bf44806f109467e166972f19e4ff8604a0590707
marcusnystrand <corporatepotato.gamedev@gmail.com>
2025-08-10 22:28:45 +0200

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,34 @@
 import type { Metadata } from 'next'
 import { GoogleAnalytics } from '@next/third-parties/google'
+import Image from 'next/image'
 import './globals.css'
 
 export const metadata: Metadata = {
   title: {
     template: '%s | Press Kit',
-    default: 'UNANNOUNCED GAME'
+    default: 'UNANNOUNCED GAME',
   },
   description: 'UNANNOUNCED GAME',
   robots: {
     index: false,
-    follow: false
-  }
+    follow: false,
+  },
 }
 
-export default async function RootLayout({
-  children
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+export default async function RootLayout() {
   return (
     <html lang="en">
       <body>
-<div className="h-screen flex flex-col items-center justify-center">
-                  <img
-                alt="Corporate Potato logo"
-                 src="/nextjs-github-pages/assets/CorporatePotato_Logo_Horizontal_White.png"
-                className="block h-5 w-auto sm:h-4"
-              />
-  <p className="text-5xl text-center">Coming soon</p>
-</div>
+        <div className="h-screen flex flex-col items-center justify-center">
+          <Image
+            alt="Corporate Potato logo"
+            src="/nextjs-github-pages/assets/CorporatePotato_Logo_Horizontal_White.png"
+            className="block h-5 w-auto sm:h-4"
+            width={300} // adjust to your image's real width
+            height={60} // adjust to your image's real height
+          />
+          <p className="text-5xl text-center">Coming soon</p>
+        </div>
       </body>
       <GoogleAnalytics gaId="G-GFP88W7X30" />
     </html>
