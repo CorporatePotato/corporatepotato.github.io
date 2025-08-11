1f73d50058885fcba5df7d11c859570cfe146807
marcusnystrand <corporatepotato.gamedev@gmail.com>
2025-08-11 20:26:31 +0200

    Update

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,17 @@
+// app/robots.ts
 import type { MetadataRoute } from 'next'
- 
+
+export const dynamic = 'force-static' // This makes it work with next export
+
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/private/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: '/private/',
+      },
+    ],
     sitemap: 'https://corporatepotato.com/sitemap.xml',
   }
 }
