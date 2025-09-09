06caf163819a063539841923ff016e0d2ccd3fe6
CorporatePotato <213948034+CorporatePotato@users.noreply.github.com>
2025-09-09 08:02:27 +0200

    Coming soon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: {
-    template: '%s | Corporate Potato',
+    template: 'Coming soon',
     default: 'Corporate Potato'
   },
   metadataBase: new URL(
