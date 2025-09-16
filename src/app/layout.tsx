232371d763367f653732233334e15bcf50af7e52
Corporate Potato <213948034+CorporatePotato@users.noreply.github.com>
2025-09-16 20:17:01 +0200

    Activated indexing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,8 @@ export const metadata: Metadata = {
     apple: '/icons/apple-touch-icon.png'
   },
   robots: {
-    index: false,
-    follow: false
+    index: true,
+    follow: true
   },
   category: 'Games'
 }
