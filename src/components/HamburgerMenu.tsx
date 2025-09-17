be995c7edb5518865f9267a1f2220f57ce1d86d6
Philip Åkerfeldt <philip.akerfeldt@swace.se>
2025-09-17 21:42:58 +0200

    feat: add transition prop to menu and prevent scroll on mobile devices

--- src/components/HamburgerMenu.tsx
+++ src/components/HamburgerMenu.tsx
@@ -2,6 +2,7 @@ import classNames from 'classnames'
 import { useRef } from 'react'
 
 import { useDictionary } from '@/context/dictionary-context'
+import { useIsMobile } from '@/hooks/useIsMobile'
 import { handlePrefillClick, navigationItems } from '@/utils/navigation'
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
@@ -9,16 +10,17 @@ import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 export const HamburgerMenu = () => {
   const hamburgerMenuCloseRef = useRef<(() => void) | null>(null)
 
+  const isMobile = useIsMobile()
+
   const dict = useDictionary()
 
   return (
     <Menu className="h-full lg:hidden" as="div">
-      {({ close }) => {
+      {({ open, close }) => {
         hamburgerMenuCloseRef.current = close
 
         return (
           <>
-            {/* Button */}
             <MenuButton className="nav hamburgerButton" aria-label="Hamburger menu button">
               {({ open }) => {
                 const icon = open ? (
@@ -31,8 +33,12 @@ export const HamburgerMenu = () => {
               }}
             </MenuButton>
 
-            {/* Drop down */}
-            <MenuItems anchor="top start" className="dropDown" modal={false}>
+            <MenuItems
+              anchor="top start"
+              className="dropDown"
+              modal={isMobile && open ? true : false}
+              transition
+            >
               {navigationItems.map((item, i, arr) => {
                 if (['nav_requestKey', 'nav_contact'].includes(item.key)) {
                   const reason = item.key === 'nav_requestKey' ? 'request-key' : 'contact'
@@ -43,8 +49,10 @@ export const HamburgerMenu = () => {
                         <button
                           className="nav dropDownItem w-full border-0 bg-transparent p-0 text-left"
                           onClick={() => {
-                            handlePrefillClick(reason)
                             itemClose()
+                            setTimeout(() => {
+                              handlePrefillClick(reason)
+                            }, 100)
                           }}
                         >
                           {dict[item.key]}
